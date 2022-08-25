67a593315c4d8e1ebfdc592764afefee6467129f
Sidor007 <Sidor007@yandex.ru>
2022-08-25 11:55:38 +0700

    jsonCorrect1

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,16 @@ app.use(requestLogger);
 app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      return res.end();
+      if (method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Headers', requestHeaders);
+        return res.end();
+      }
     }
   }
   return next();
